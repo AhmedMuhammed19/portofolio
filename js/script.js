a44e82a0a31f507cb8bf318cecfa4ddd73e4c1df
ahmed <ahmedmobuss@gmail.com>
2023-09-03 22:55:24 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,119 @@
+let toggleBtn = document.querySelector("#night-mode-btn");
+let body = document.querySelector("body");
+let darkMode = false;
+
+toggleBtn.addEventListener("change", (event) => {
+  darkMode = event.target.checked;
+  if (darkMode) {
+    body.classList.add("dark");
+  } else {
+    body.classList.remove("dark");
+  }
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const phrases = [
+  "Graphic Designer",
+  "UI Designer",
+  "Visual identity Designer",
+  "Freelancer",
+];
+const el = document.getElementById("type-writer");
+
+let sleepTime = 150;
+
+let curPhraseIndex = 0;
+
+const writeLoop = async () => {
+  while (true) {
+    let curWord = phrases[curPhraseIndex];
+
+    for (let i = 0; i < curWord.length; i++) {
+      el.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 10);
+
+    for (let i = curWord.length; i > 0; i--) {
+      el.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 5);
+
+    if (curPhraseIndex === phrases.length - 1) {
+      curPhraseIndex = 0;
+    } else {
+      curPhraseIndex++;
+    }
+  }
+};
+
+writeLoop();
+
+let sectionsScroll = document.querySelectorAll("section");
+let navLinksScroll = document.querySelectorAll(".landing nav a");
+
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+var sections = document.querySelectorAll("section");
+
+onscroll = function () {
+  var scrollPosition = document.documentElement.scrollTop;
+
+  sections.forEach((section) => {
+    if (
+      scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+      scrollPosition <
+        section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
+    ) {
+      var currentId = section.attributes.id.value;
+      removeAllActiveClasses();
+      addActiveClass(currentId);
+    }
+  });
+};
+
+var removeAllActiveClasses = function () {
+  document.querySelectorAll("nav a").forEach((el) => {
+    el.classList.remove("active");
+  });
+};
+
+var addActiveClass = function (id) {
+  // console.log(id);
+  var selector = `nav a[href="#${id}"]`;
+  document.querySelector(selector).classList.add("active");
+};
+
+var navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    var currentId = e.target.attributes.href.value;
+    var section = document.querySelector(currentId);
+    var sectionPos = section.offsetTop;
+    // section.scrollIntoView({
+    //   behavior: "smooth",
+    // });
+
+    window.scroll({
+      top: sectionPos,
+      behavior: "smooth",
+    });
+  });
+});
+
